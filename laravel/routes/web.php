2eb729c4297e16fad3d2595cabfffbb34a3d7434
ida-work <i21155388@gmail.com>
2025-06-11 21:25:36 +0900

    Laravelプロジェクトに新しい依存関係を追加し、テストケースを改善。composer.jsonにlaravel/breeze、pest、pest-plugin-laravelを追加し、package.jsonにtailwindcss、alpinejs、autoprefixerを追加。CSSファイルをtailwindの形式に変更し、JSファイルにAlpine.jsの初期化を追加。ルーティングを更新し、ProfileControllerを使用してユーザープロファイルの管理を実装。テストケースを簡素化し、期待される結果を確認するための新しい構文を導入。

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-use Illuminate\Support\Facades\DB;;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/db-test', function () {
-    try {
-        DB::connection()->getPdo();
-        return "Connected successfully to the database.";
-    } catch (\Exception $e) {
-        return "Could not connect to the database. " . $e->getMessage();
-    }
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
