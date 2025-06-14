50a032cc79f5432e7648cc5e1f7eb8f5a19fbefc
ida-work <i21155388@gmail.com>
2025-06-14 13:28:30 +0900

    ユーザー管理のための新しいルートを追加し、UserControllerを使用してユーザー一覧を表示する機能を実装。

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
